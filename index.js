8f3be1fb307a00d1a4e4598635b270e66e054830
MasrulSakib <Masrul.Sakib69@gmail.com>
2024-05-08 22:38:15 +0600

    services data added to mongoDB and fetched to client

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster1.m4sihj5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const serviceCollection = client.db('LoveLens').collection('services')
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+
+        })
+
+    } finally {
+
+    }
+}
+run().catch(error => console.error(error));
+
 
 app.get('/', (req, res) => {
     res.send('love lens is running successfully')
