56f0086b93ea2331a0b364e88fcd5e1f9ba0c2a7
MasrulSakib <Masrul.Sakib69@gmail.com>
2024-05-09 02:12:00 +0600

    specific service route created and loaded from mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,10 +27,25 @@ async function run() {
 
         app.get('/services', async (req, res) => {
             const query = {};
+            const cursor = serviceCollection.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services);
+
+        })
+
+        app.get('/allServices', async (req, res) => {
+            const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
 
+        })
+
+        app.get(`/services/:id`, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
         })
 
     } finally {
